0ff1ad5865f3fd493891dd11a9764a0082de5b78
trunganh1100 <trunganh1100@gmail.com>
2024-05-09 21:50:39 +0700

    new commit

--- adapter/connection/MongoDB.go
+++ adapter/connection/MongoDB.go
@@ -2,9 +2,6 @@ package connection
 
 import (
 	"context"
-	"encoding/base64"
-	"fmt"
-	"os"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -13,18 +10,7 @@ import (
 
 func Connn() *mongo.Database {
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-	basecodeuri := os.Getenv("MONGO_URI")
-	if basecodeuri == "" {
-		// Xử lý trường hợp không tìm thấy biến môi trường
-		fmt.Println("NO ENV variable")
-	}
-	decodeuri, err := base64.StdEncoding.DecodeString(basecodeuri)
-	if err != nil {
-		// Xử lý lỗi khi giải mã
-		fmt.Println("Lỗi khi giải mã base64:", err)
-	}
-	uri := string(decodeuri)
-	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+	opts := options.Client().ApplyURI("mongodb+srv://trunganh1100:baocaomonhoc123@cluster0.26abo3u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").SetServerAPIOptions(serverAPI)
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	client, err := mongo.Connect(ctx, opts)
